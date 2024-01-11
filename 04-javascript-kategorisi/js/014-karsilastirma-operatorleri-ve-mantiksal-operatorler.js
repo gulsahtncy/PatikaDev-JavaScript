78a04d2850fa8119e3a3c762100d6e5605f73713
Gülşah <gulsahtuncay1@gmail.com>
2024-01-11 22:14:22 +0300

    Adding new file

--- 04-javascript-kategorisi/js/014-karsilastirma-operatorleri-ve-mantiksal-operatorler.js
+++ 04-javascript-kategorisi/js/014-karsilastirma-operatorleri-ve-mantiksal-operatorler.js
@@ -1,41 +1,41 @@
 // ********** Karsilastirma Operatorleri ve Mantiksal Operatorler **********
 // https://www.w3schools.com/js/js_comparisons.asp
 
-let price = "0"
+let price = "100"
 let user = "hakan"
 
 // == Eşitse
-console.log("== :", price == 1 )
-console.log("== :", price == 100 )
+console.log("== :", price == 1 ) // false
+console.log("== :", price == 100 ) // true
 
 // === Hem değeri hem de türü eşitse
-console.log("=== :", price === 1 )
-console.log("=== :", price === 100 )
+console.log("=== :", price === 1 ) // false
+console.log("=== :", price === 100 ) // false
 
 // != Eşit değilse
-console.log(user != "guest" )
+console.log(user != "guest" ) // true
 
 // < Küçükse
-console.log("price < 100", price < 100)
+console.log("price < 100", price < 100) // false
 
 // <= Küçük veya eşitse
-console.log("price <= 100", price <= 100)
+console.log("price <= 100", price <= 100) // true
 
 // > Büyükse
-console.log("price > 200", price > 200)
+console.log("price > 200", price > 200) // false
 
 // >= Büyük veya eşitse
-console.log("price >= 100", price >= 100)
+console.log("price >= 100", price >= 100) // true
 
 
 // && ve
 price = 0
-console.log( price > 0 && user != "guest" )
+console.log( price > 0 && user != "guest" ) // false
 
 // || veya
-console.log( price > 0 || user != "guest" )
+console.log( price > 0 || user != "guest" ) // true
 
 // ! degil (tersi)
 user = "guest"
 price = 1
-console.log( price > 0 && !user == "guest" )
+console.log( price > 0 && !user == "guest" ) // false
