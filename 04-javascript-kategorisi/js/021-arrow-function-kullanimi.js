3bbdeb1ab140d073bf025112531cd3d005fc9ce9
Gülşah <gulsahtuncay1@gmail.com>
2024-01-15 18:30:02 +0300

    Adding new file

--- 04-javascript-kategorisi/js/021-arrow-function-kullanimi.js
+++ 04-javascript-kategorisi/js/021-arrow-function-kullanimi.js
@@ -2,29 +2,43 @@
 // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Functions/Arrow_functions
 
 function hello(firstName) {
-    console.log(`Merhaba ${firstName}`)
+  console.log(`Merhaba ${firstName}`);
 }
 
-hello("JavaScript")
+hello("JavaScript");
 
-const helloFuncV1 = (firstName) => { console.log(`Merhaba ${firstName}`) } 
-helloFuncV1("helloFuncV1")
+const helloFuncV1 = (firstName) => {
+  console.log(`Merhaba ${firstName}`);
+};
+helloFuncV1("helloFuncV1");
 
+const helloFuncV2 = (firstName) => console.log(`Merhaba ${firstName}`); // bir parametre, bir donus islemi
+helloFuncV2("helloFuncV2");
 
-const helloFuncV2 = firstName => console.log(`Merhaba ${firstName}`) // bir parametre, bir donus islemi
-helloFuncV2("helloFuncV2")
+const helloFuncV3 = (firstName, lastName) =>
+  console.log(`Merhaba ${firstName} ${lastName}`); // console.log parantezi
 
-
-const helloFuncV3 = (firstName, lastName) => console.log(
-    `Merhaba ${firstName} ${lastName}`
-) // console.log parantezi
-
-helloFuncV3("helloFuncV3", "Last Name info")
+helloFuncV3("helloFuncV3", "Last Name info");
 
 const helloFuncV4 = (firstName, lastName) => {
-    let info = `Merhaba ${firstName} ${lastName}`
-    console.log(info) 
-    return info
-}
+  let info = `Merhaba ${firstName} ${lastName}`;
+  console.log(info);
+  return info;
+};
 
-helloFuncV4("helloFuncV4", "Other Info")
+helloFuncV4("helloFuncV4", "Other Info");
+
+// this function
+let movie = {
+  name: "La la land",
+
+  thisInArrow: () => {
+    console.log("Movie name is " + this.name); // 'this' window'u referans gösterir. Bu yüzden name'yi bulamaz.
+  },
+
+  thisInRegular() {
+    console.log("Movie name is " + this.name); // 'this' kendisini referans gösterir ve çalışır.
+  },
+};
+movie.thisInArrow(); // output : Movie name is
+movie.thisInRegular(); // output : Movie name is La la land
--- 04-javascript-kategorisi/js/022-dom-etkinlikleriyle-calismak.js
+++ 04-javascript-kategorisi/js/022-dom-etkinlikleriyle-calismak.js
@@ -2,10 +2,96 @@
 // https://www.w3schools.com/jsref/dom_obj_event.asp
 // https://developer.mozilla.org/en-US/docs/Web/Events
 
-let greeting = document.querySelector("#greeting")
-greeting.addEventListener("click", domClick)
+let greeting = document.querySelector("#greeting");
+greeting.addEventListener("click", domClick);
 
 function domClick() {
-    console.log("etkinlik denetlendi")
-    this.style.color == "red" ? this.style.color = "black" : this.style.color = "red"
+  console.log("etkinlik denetlendi");
+  this.style.color == "red"
+    ? (this.style.color = "black")
+    : (this.style.color = "red");
 }
+
+// 1.METOT
+element.addEventListener(eventt, function () {
+  // Nesneye bir etkinlik atandı..
+  const button = document.querySelector("#button");
+  //Burada ilk olarak tip ('click') ,ikinci olarak da fonksiyonun kendisi parametre olarak eklenir
+  button.addEventListener("click", function () {
+    alert("Buton Tıklandı");
+  });
+  // Çıktı : Buton Tıklandı!!!
+});
+
+// 2.METOT
+element.addEventListener(eventt, functionName); // Fonksiyon etkinlik parametresi olarak da çalışabilmektedir.
+//id =button olan nesne seçildi
+const button = document.querySelector("#button");
+//nesneye bir etkinlik atandı.Burada ilk olarak tip ('click')
+//ikinci olarak da aşağıda fonksiyon tanımlanır ,fonksiyon ismi parametre olarak girilir.
+button.addEventListener("click", btnClicked);
+function btnClicked() {
+  alert("Buton Tıklandı!!!");
+}
+// Çıktı : Buton Tıklandı!!!
+
+// MOUSE ETKİNLİĞİ
+//id = text olan buton çağrıldı
+//dblclick = çift tıklama etkinliği nesneye atandı, fonksiyon ismi parametre olarak tanımlandı
+const buttonn = document.querySelector("#text");
+button.addEventListener("dblclick", clickFonksiyonu);
+//fonksiyonda id = text olan element çağrılarak innerHTML ile içeriği değiştirildi.
+function clickFonksiyonu() {
+  document.getElementById("text").innerHTML = "NEW FORM";
+}
+
+// KLAVYE ETKİNLİĞİ
+//id = fname olan nesne çağrılarak keyboard değişkenine atandı
+const keyboard = document.querySelector("#fname");
+//keyboard isimli değişkene "cut" etkinliği atandı ve fonksiyon ismi parametre olarak atandı
+keyboard.addEventListener("cut", cuttingFunction);
+//cuttingFunction isimli fonksiyon ile form inputuna yazılan girdileri klavye ctrl+c ile kesildiğinde etkinlik çalışmaktadır
+function cuttingFunction(event) {
+  console.log("etkinlik tipi: ", event.type);
+}
+//console ekranı : etkinlik tipi: cut
+
+// FORM ETKİNLİKLERİ
+<form action="#" id="selectColor">
+  <label for="color">Choose a color</label>
+  <select name="colors" id="select">
+    <option value="red">RED</option>
+    <option value="blue">BLUE</option>
+    <option value="purple">PURPLE</option>
+    <option value="orange">ORANGE</option>
+    <option value="pink">PINK</option>
+  </select>
+  <br></br>
+  <input type="submit" value="Submit"></input>
+</form>;
+
+//id=select olan nesne çağrılarak color değişkenine atandı
+const color = document.querySelector("#select");
+//color değişkenine change etkinliği etkinliği atandı ve fonksiyon ismi parametre olarak atandı
+color.addEventListener("change", selectBox);
+
+//selectBox isimli fonksiyon ile selectbox her seçildiğinde etkinlik tipi =change ve değeri consola yazılmaktadır
+function selectBox(event) {
+  console.log("Etkinlik tipi : " + event.type);
+  console.log("deger : " + event.target.value);
+}
+// Selectbox içinde "blue" değerini seçtiğimiz takdirde;
+// ekran çıktısı: Etkinlik tipi : change, deger : blue
+
+// Kabarcıklanma Etkinliği (Event Bubbling) ve Yakalama Etkinliği (Event Capturing)
+    // 1.Kabarcıklanma Etkinliği(Event Bubbling)
+    element.addEventListener(eventt, function(){}); // Örnekte event parametresi "click" etkinliği almaktadır. function parametresi ise fonksiyonun ismini veya fonksiyonun kendisini almaktadır.
+
+    // 2.Yakalama Etkinliği(Event Capturing)
+    element.addEventListener(eventt, function(){}, useCapture); // useCapture parametresi "True" ve "False" değerleri almaktadır. Diğer tüm kullanımı kabarcıklanma etkinliği ile aynıdır.
+
+    // event.stopPropagation() // Bir etkinliğin özelliğini durdurmak için kullanılır. Bir fonksiyon parametresi olarak girilen "event" e bu etkinlik atanarak kullanılır.
+    // event.preventDefault(); // Bir HTML elementinin varsayılan eylemini tamamen engellemek için bu eylem kullanılır. Bir fonksiyon parametresi olarak girilen "event" e bu etkinlik atanarak kullanılır.
+    
+
+
